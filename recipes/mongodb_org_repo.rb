ba5508af6eacaf1c1456417aaa7e2437fc48da06
Kenneth Yang (DCS-TW) <kenneth_yang@tw.trendnet.org>
2016-05-12 19:04:41 +0800

    disable platform selection

--- recipes/mongodb_org_repo.rb
+++ recipes/mongodb_org_repo.rb
@@ -23,28 +23,28 @@
 # http://www.mongodb.org/downloads#packages
 node.override['mongodb']['package_name'] = 'mongodb-org'
 
-case node['platform_family']
-when 'debian'
+#case node['platform_family']
+#when 'debian'
   # Adds the repo: http://www.mongodb.org/display/DOCS/Ubuntu+and+Debian+packages
-  apt_repository 'mongodb' do
-    uri "http://downloads-distro.mongodb.org/repo/#{node[:mongodb][:apt_repo]}"
-    distribution 'dist'
-    components ['10gen']
-    keyserver 'hkp://keyserver.ubuntu.com:80'
-    key '7F0CEB10'
-    action :add
-  end
+#  apt_repository 'mongodb' do
+#    uri "http://downloads-distro.mongodb.org/repo/#{node[:mongodb][:apt_repo]}"
+#    distribution 'dist'
+#    components ['10gen']
+#    keyserver 'hkp://keyserver.ubuntu.com:80'
+#    key '7F0CEB10'
+#    action :add
+#  end
 
-when 'rhel', 'fedora'
-  yum_repository 'mongodb' do
+#when 'rhel', 'fedora'
+  yum_repository 'mongodb-org' do
     description 'mongodb RPM Repository'
     baseurl "http://downloads-distro.mongodb.org/repo/redhat/os/#{node['kernel']['machine']  =~ /x86_64/ ? 'x86_64' : 'i686'}"
     action :create
     gpgcheck false
     enabled true
-  end
+#  end
 
-else
+#else
   # pssst build from source
-  Chef::Log.warn("Adding the #{node['platform_family']} 10gen repository is not yet not supported by this cookbook")
-end
+#  Chef::Log.warn("Adding the #{node['platform_family']} 10gen repository is not yet not supported by this cookbook")
+#end
